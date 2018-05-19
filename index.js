f9a7056bdbb10c4d0f534233ab480c857ee00912
Timur <timur.code@gmail.com>
2018-05-19 11:36:09 +0200

    add mongo

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const http = require('http')
 const port = 3000
 
 const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
+  console.log(request.url);
+  console.log(request.method);
+  console.log(request);
+  response.end('Hello Node.js Server!');
 }
 
 const server = http.createServer(requestHandler)
