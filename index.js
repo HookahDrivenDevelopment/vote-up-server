a63e78b511a36d07a315694723b3f22f032aedc6
Timur <timur.code@gmail.com>
2018-06-13 02:28:15 +0200

    added some basic functionality

--- index.js
+++ index.js
@@ -2,35 +2,46 @@ const http = require('http')
 const express = require ('express')
 const port = 3000
 const app = express();
+const MongoClient = require('mongodb').MongoClient
 var bodyParser = require("body-parser");
+var db;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const requestHandler = (request, response) => {
-  console.log(request.url);
-  console.log(request.method);
-  console.log(request);
-  response.end('Hello Node.js Server!');
-}
-
-app.get('/govno', (req, res, next)=>{
-  res.send({gonvo:"govno"})
-})
-
-app.get('/*', (req, res, next)=>{
-  res.send({a:"govno"})
+app.get('/', (req, res) => {
+  db.collection('votes').find().toArray((err, result) => {
+    console.log("result = ", result);
+    if (err) return console.log(err)
+    res.send({votes: result})
+  })
 })
 
 app.post('/', (req, res, next) => {
   var body = req.body;
+  db.collection('votes').save(body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+  })
   console.log("body = ", body);
 })
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
+const dbUrl = "mongodb://<user>:<password>@ds257590.mlab.com:57590/votes";
+MongoClient.connect(dbUrl, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('votes')
 
-  console.log(`server is listening on ${port}`)
+  app.listen(port, (err) => {
+    if (err) {
+      return console.log('something bad happened', err)
+    }
+  
+    console.log(`server is listening on ${port}`)
+  })
 })
+
+
+
+
+
