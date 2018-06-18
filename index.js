8024c1d3cc27768dfe057f6e6ad68410d073ba3c
Timur <timur.code@gmail.com>
2018-06-18 02:27:06 +0200

    add smth

--- index.js
+++ index.js
@@ -9,25 +9,34 @@ var db;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  db.collection('votes').find().toArray((err, result) => {
-    console.log("result = ", result);
-    if (err) return console.log(err)
-    res.send({votes: result})
-  })
-})
-
 app.post('/', (req, res, next) => {
   var body = req.body;
   db.collection('votes').save(body, (err, result) => {
-    if (err) return console.log(err)
-
-    console.log('saved to database')
+    if (err) return console.log(err);
+    console.log('saved to database');
+    res.send({});
   })
-  console.log("body = ", body);
 })
 
-const dbUrl = "mongodb://<user>:<password>@ds257590.mlab.com:57590/votes";
+app.put('/', (req, res) => {
+  var body = req.body;
+  var query = { id: body.id };
+  var value = { $inc: { [`answers.${body.answerId}.voted`]: 1, voted: 1 }};
+
+  db.collection('votes').updateOne(query, value, (err, result) => {
+    if (err) return console.log(err);
+    res.send({});
+  })
+})
+
+app.get('/', (req, res) => {
+  db.collection('votes').find().toArray((err, result) => {
+    if (err) return console.log(err);
+    res.send({votes: result});
+  })
+})
+
+const dbUrl = "mongodb://murdoc:d77_deVil@ds257590.mlab.com:57590/votes";
 MongoClient.connect(dbUrl, (err, client) => {
   if (err) return console.log(err)
   db = client.db('votes')
